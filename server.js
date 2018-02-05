f3610aa621087ce2e5e4513926bacea11e23e0c4
Alexandra Swart <answart@sbcglobal.net>
2018-02-05 02:40:46 -0800

    Add connect-mongo package

--- server.js
+++ server.js
@@ -10,26 +10,10 @@ const express    = require('express'),
   mongoose       = require('mongoose'),
   bodyParser     = require('body-parser'),
   session        = require('express-session'),
+  MongoStore     = require('connect-mongo')(session),
   cookieParser   = require('cookie-parser'),
   flash          = require('connect-flash');
 
-
-// Set sessions and cookie parser
-app.use(cookieParser());
-app.use(session({
-  secret: 'my-super-secret',
-  cookie: { maxAge: 6000 },
-  resave: false, // forces session to be saved to store
-  saveUninitialized: false // dont save unmodified
-}));
-app.use(flash());
-
-// Set public directories
-app.use('/public', express.static(__dirname + '/public'));
-
-// Set ejs as templating engine
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
 
 // Connect to db
 mongoose.connect(process.env.DB_URI);
@@ -38,6 +22,31 @@ mongoose.connection.on('error', function(err) {
     process.exit(-1);
   }
 );
+
+app.set('trust proxy', 1);
+// Set sessions and cookie parser
+app.use(cookieParser());
+app.use(session({
+  secret: 'my-super-secret',
+  cookie: { maxAge: 6000 },
+  resave: false, // forces session to be saved to store
+  saveUninitialized: false, // dont save unmodified
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+app.use(function(req,res,next){
+  if(!req.session){
+    return next(new Error('Oh no! No session found.')) //handle error
+  }
+  next();
+});
+app.use(flash());
+
+// Set public directories
+app.use('/public', express.static(__dirname + '/public'));
+
+// Set ejs as templating engine
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
